87465f24238b2a6a2a43e3ac1ef56f4e4c6efda0
yevhenii <yevheniiskoryi@gmail.com>
2019-09-09 14:41:35 +0300

    added init.sql, run as container with creation database(liquibase-core)

--- src/test/java/com/example/country/CountryApplicationTests.java
+++ src/test/java/com/example/country/CountryApplicationTests.java
@@ -1,18 +1,37 @@
 package com.example.country;
 
+import com.example.country.dto.CountryDTOOut;
 import com.example.country.exception.CountryNotFoundException;
 import com.example.country.exception.LanguageNotFoundException;
+import com.example.country.exception.TranslateNotFoundException;
+import com.example.country.repository.CountryRepository;
+import com.example.country.repository.LanguageRepository;
+import com.example.country.service.CountryServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Optional;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CountryApplicationTests {
 
+
+    @Mock
+    private CountryRepository countryRepository;
+
+    @Mock
+    private LanguageRepository languageRepository;
+
+    @InjectMocks
+    private CountryServiceImpl countryService;
 
     @Test
     public void contextLoads() {
@@ -21,42 +40,40 @@ public class CountryApplicationTests {
 
     @Test
     public void testGetCountryWithCode001AndLocalizationEN(){
-//        List<Language> languages = new ArrayList<>();
-//        languages.add(new Language("EN","Ukraine"));
-//        Country country = new Country("001");
-//        country.setLanguages(languages);
-//
-//        Optional<Country> optionalCountry = Optional.of(country);
-//        when(countryRepository.findByCode("001")).thenReturn(optionalCountry);
-//
-//        String nameOfCountry = countryService.getCountryNameByCodeAndLocalization("001","EN");
-//
-//        assertEquals("Ukraine", nameOfCountry);
+
+        when(countryRepository.getIdByCode("001")).thenReturn(Optional.of(1));
+        when(languageRepository.getIdByLocalization("EN")).thenReturn(Optional.of(1));
+        when(countryRepository.getTranslateByCountryIdAndLanguageId(1,1)).thenReturn(Optional.of("Ukraine"));
+
+        CountryDTOOut country = countryService.getCountryNameByCodeAndLocalization("001","EN");
+
+        CountryDTOOut countryDTOOut = new CountryDTOOut("Ukraine");
+        assertEquals(countryDTOOut, country);
     }
 
     @Test(expected = CountryNotFoundException.class)
-    public void testGetCountryWithCode003AndLocalizationEN(){
-//        List<Language> languages = new ArrayList<>();
-//        languages.add(new Language("EN","Ukraine"));
-//        Country country = new Country("001");
-//        country.setLanguages(languages);
-//
-//        Optional<Country> optionalCountry = Optional.of(country);
-//        when(countryRepository.findByCode("001")).thenReturn(optionalCountry);
-//
-//        String nameOfCountry = countryService.getCountryNameByCodeAndLocalization("003","EN");
+    public void testGetCountryWithCode004AndLocalizationEN(){
+        when(countryRepository.getIdByCode("004")).thenThrow(CountryNotFoundException.class);
+
+        CountryDTOOut country = countryService.getCountryNameByCodeAndLocalization("004","EN");
     }
 
     @Test(expected = LanguageNotFoundException.class)
     public void testGetCountryWithCode001AndLocalizationFV(){
-//        List<Language> languages = new ArrayList<>();
-//        languages.add(new Language("EN","Ukraine"));
-//        Country country = new Country("001");
-//        country.setLanguages(languages);
-//
-//        Optional<Country> optionalCountry = Optional.of(country);
-//        when(countryRepository.findByCode("001")).thenReturn(optionalCountry);
-//
-//        String nameOfCountry = countryService.getCountryNameByCodeAndLocalization("001","FI");
+        when(countryRepository.getIdByCode("001")).thenReturn(Optional.of(1));
+        when(languageRepository.getIdByLocalization("FV")).thenThrow(LanguageNotFoundException.class);
+
+        CountryDTOOut country = countryService.getCountryNameByCodeAndLocalization("001","FV");
+    }
+
+
+    @Test(expected = TranslateNotFoundException.class)
+    public void testGetCountryWithCode003AndLocalizationEN(){
+
+        when(countryRepository.getIdByCode("003")).thenReturn(Optional.of(3));
+        when(languageRepository.getIdByLocalization("EN")).thenReturn(Optional.of(1));
+        when(countryRepository.getTranslateByCountryIdAndLanguageId(3,1)).thenThrow(TranslateNotFoundException.class);
+
+        CountryDTOOut country = countryService.getCountryNameByCodeAndLocalization("003","EN");
     }
 }
